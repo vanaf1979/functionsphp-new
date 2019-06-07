5c15a4f70a08b15358bcb61c8b6e7272afd7ebc5
Stephan <vanaf1979@gmail.com>
2019-06-07 20:58:48 +0200

    init repo

--- functionsphp/functionsphp.php
+++ functionsphp/functionsphp.php
@@ -6,6 +6,9 @@ namespace FunctionsPhp;
 class FunctionsPhp {
 
 
+    /**
+     * the constructor.
+     */
     public function __construct() {
 
         $this->register_constants();
@@ -13,6 +16,13 @@ class FunctionsPhp {
     }
 
 
+    /**
+     * get_service_classes.
+     *
+     * Store and return all services classes.
+     *
+     * @return array
+     */
     public function get_service_classes(): array {
 
         return [
@@ -24,6 +34,13 @@ class FunctionsPhp {
     }
 
 
+    /**
+     * register_constants.
+     *
+     * Register all constants for this theme.
+     *
+     * @return void
+     */
     private function register_constants() : void {
 
         $theme = \wp_get_theme();
