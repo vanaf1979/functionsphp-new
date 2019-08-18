f00127096d830642266944a05ac1d46541d4988a
Stephan <vanaf1979@gmail.com>
2019-08-18 12:06:18 +0200

    refactored transient class

--- functionsphp/dependencies/Transients.php
+++ functionsphp/dependencies/Transients.php
@@ -1,21 +1,20 @@
 <?php
+/**
+ * Transients class.
+ * 
+ * Wrapper class to handle WordPress transients.
+ *
+ * @package   FunctionsPhp\Dependencies
+ * @author    Stephan Nijman.
+ * @license   MIT
+ * @link      https://vanaf1979.nl
+ * @copyright 2019 Stephan Nijman.
+ */
 
 namespace FunctionsPhp\Dependencies;
 
 
-use \stdClass;
-
-
 final class Transients {
-
-    // Time constants
-    // MINUTE_IN_SECONDS
-    // HOUR_IN_SECONDS
-    // DAY_IN_SECONDS
-    // WEEK_IN_SECONDS
-    // MONTH_IN_SECONDS
-    // YEAR_IN_SECONDS
-
 
     /**
      * the constructor.
@@ -27,26 +26,54 @@ final class Transients {
      * set_transient.
      *
      * Set transient with data and experation time.
+     * 
+     * @uses set_transient https://developer.wordpress.org/reference/functions/set_transient/
      *
-     * @return void
+     * @param string $transient transient name.
+     * @param string $data transient data.
+     * @param string $time transient experation time.
+     * 
+     * @return bool
      */
-    public function set_transient( string $transient = null , string $data = null , string $time = YEAR_IN_SECONDS ) {
+    public function set_transient( string $transient = null , string $data = null , string $time = YEAR_IN_SECONDS ) : bool {
 
-        \set_transient( $transient , $data , $time );
-
+        return \set_transient( $transient , $data , $time );
+        
     }
 
 
     /**
      * get_transient.
      *
-     * Set transient with data and experation time.
+     * Get transient from database.
+     * 
+     * @uses get_transient https://developer.wordpress.org/reference/functions/get_transient/
      *
-     * @return void
+     * @param string $transient transient name.
+     * 
+     * @return mixed
      */
     public function get_transient( string $transient = null ) {
 
-        \get_transient( $transient );
+        return \get_transient( $transient );
+
+    }
+
+
+    /**
+     * delete_transient.
+     *
+     * delete transient from database.
+     * 
+     * @uses delete_transient https://developer.wordpress.org/reference/functions/delete_transient/
+     *
+     * @param string $transient transient name.
+     * 
+     * @return bool
+     */
+    public function delete_transient( string $transient = null ) : bool {
+
+        return \delete_transient( $transient );
 
     }
 
@@ -54,11 +81,17 @@ final class Transients {
     /**
      * optional.
      *
-     * Check if transient exists, else run callback function and store transient.
+     * Check if transient exists, else run callback function and store transient data.
+     * 
+     * @uses call_user_funtion https://www.php.net/manual/en/function.call-user-func.php
      *
-     * @return void
+     * @param string $transient transient name.
+     * @param string $data transient data.
+     * @param string $time transient experation time.
+     * 
+     * @return mixed
      */
-    public function optional( string $transient = null , callable $callback = null , string $time = YEAR_IN_SECONDS ) : string {
+    public function optional( string $transient = null , callable $callback = null , string $time = YEAR_IN_SECONDS ) {
 
         if( '' != $transient_data = $this->get_transient( $transient ) ) {
 
